7600dc58612a93914e394cd14b618c918382eeb5
@gmgower <gmgower@gmail.com>
2020-08-16 20:08:45 -0500

    Initial setup ecommerce backend.

---
+++ src/index.server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const env = require('dotenv');
+const app = express();
+
+// environment variable 
+env.config();
+
+app.get('/', (req,res, next) => {
+    res.status(200).json({
+        message: 'Hello from Server'
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
